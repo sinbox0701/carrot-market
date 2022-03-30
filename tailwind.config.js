28504771e680d66597b7ad0490b52fb56c27d420
sinbox0701 <psh090953@gmail.com>
2022-03-30 11:13:35 +0900

    Set Up

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}"
+  ], // pages아래의 모든 파일(js,jsx,ts,tsx)에 tailwind적용
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
