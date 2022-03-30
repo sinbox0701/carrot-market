da557f3806087051cbc11e962f9df26505ad3f60
sinbox0701 <psh090953@gmail.com>
2022-03-30 22:11:07 +0900

    Tailwind P2

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@ import type { NextPage } from 'next'
 
 const Home: NextPage = () => {
   return (
-    <div className='bg-slate-400 py-20 px-10 flex flex-col space-y-5'>
+    <div className='bg-slate-400 py-20 px-20 flex flex-col space-y-5 min-h-screen'>
       <div className='bg-white p-6 rounded-3xl shadow-xl'>
         <span className='font-semibold text-3xl'>Select Item</span>
         <div className='flex justify-between my-2'>
@@ -19,9 +19,58 @@ const Home: NextPage = () => {
         </div>
         <div className='mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-2/4 mx-auto'>Checkout</div>
       </div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
-      <div className='bg-white p-10 rounded-2xl shadow-xl'></div>
+      <div className='bg-white overflow-hidden rounded-3xl shadow-xl'>
+        <div className='bg-blue-500 p-6 pb-14'>
+          <span className='text-white text-2xl'>Profile</span>
+        </div>
+        <div className='rounded-3xl p-6 bg-white relative -top-5'>
+          <div className='flex relative -top-16 items-end justify-between'>
+            <div className='flex flex-col items-center'>
+              <span className='text-sm text-gray-500'>Orders</span>
+              <span className='font-medium'>340</span>
+            </div>
+            <div className='h-24 w-24 bg-red-400 rounded-full'/>
+            <div className='flex flex-col items-center'>
+              <span className='text-sm text-gray-500'>Spent</span>
+              <span className='font-medium'>$2,310</span>
+            </div>
+          </div>
+          <div className='relative flex flex-col items-center -mt-10 -mb-5'>
+            <span className='text-lg font-medium'>Tony Molly</span>
+            <span className='text-sm text-gray-500'>미국</span>
+          </div>
+        </div>
+      </div>
+      <div className='bg-white p-6 rounded-3xl shadow-xl'>
+        <div className='flex mb-5 justify-between items-center'>
+          <span>⬅️</span>
+          <div className='space-x-3'>
+            <span>⭐️ 4.9</span>
+            <span className='shadow-xl p-2 rounded-md'>❤️</span>
+          </div>
+        </div>
+        <div className='bg-zinc-400 h-72 mb-5'/>
+        <div className='flex flex-col'>
+          <span className='font-medium mb-1.5 text-xl'>Swoon Lounge</span>
+          <span className='text-xs text-gray-500'>Chair</span>
+          <div className='mt-3 mb-5 flex justify-between items-center'>
+            <div>
+              <input type='radio' />
+              <input type='radio' />
+              <input type='radio' />
+            </div>
+            <div className='flex items-center space-x-5'>
+              <button className='bg-blue-200 rounded-lg flex justify-center items-center aspect-square w-8 text-xl text-gray-500'>-</button>
+              <span>1</span>
+              <button className='bg-blue-200 rounded-lg flex justify-center items-center aspect-square w-8 text-xl text-gray-500'>+</button>
+            </div>
+          </div>
+          <div className='flex justify-between items-center'>
+            <span className='font-medium text-2xl'>$450</span>
+            <button className='bg-blue-500 py-2 px-8 text-center text-xs text-white rounded-lg'>Add to cart</button>
+          </div>
+        </div>
+      </div>
     </div>
   ); 
 }
