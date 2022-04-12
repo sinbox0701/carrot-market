1b9e3166823a19416ad60f5038af7a16a941aea4
sinbox0701 <psh090953@gmail.com>
2022-04-12 10:44:28 +0900

    Token Logic

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -3,20 +3,26 @@ import withHandler from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 
 async function handler(req:NextApiRequest, res:NextApiResponse){
-    const {phone, email} = req.body; //phone or email 중 하나
-    const payload = phone ? { phone: +phone } : { email };
-    //+phone --> "1234" -> 1234
-    const user = await client.user.upsert({//upsert ==> create or update
-      where: {
-        ...payload,
-      },
-      create: {
-        name: "Anonymous",
-        ...payload,
-      },
-      update: {},
+    const {phone, email} = req.body;
+    const user = phone ? { phone: +phone } : { email };
+    const payload = Math.floor(100000 + Math.random() * 900000) + "";
+    const token = await client.token.create({
+        data: {
+            payload,
+            user: {
+                connectOrCreate: {
+                    where: {
+                        ...user,
+                    },
+                    create: {
+                        name: "Anonymous",
+                        ...user,
+                    },
+                },
+            },
+        },
     });
-
+    
     return res.status(200).end();
 };
 
