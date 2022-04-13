1dd7e7332a0a86e267710db6d87ceeeda0faa4bb
sinbox0701 <psh090953@gmail.com>
2022-04-13 10:38:34 +0900

    Sending SMS

--- pages/api/users/enter.tsx
+++ pages/api/users/enter.tsx
@@ -2,12 +2,13 @@ import client from "@libs/server/client";
 import withHandler, { ResponseType } from "@libs/server/withHandler";
 import { NextApiRequest, NextApiResponse } from "next";
 import twilio from "twilio";
+import nodemailer from "nodemailer";
 
 const twilioClient = twilio(process.env.TWILIO_SID,process.env.TWILIO_TOKEN);
 
 async function handler(req:NextApiRequest, res:NextApiResponse<ResponseType>){
     const {phone, email} = req.body;
-    const user = phone ? { phone: +phone } : email ? { email } : null;
+    const user = phone ? { phone: phone } : email ? { email } : null;
     if(!user) return res.status(400).json({ok:false});
 
     const payload = Math.floor(100000 + Math.random() * 900000) + "";
@@ -32,6 +33,23 @@ async function handler(req:NextApiRequest, res:NextApiResponse<ResponseType>){
             messagingServiceSid: process.env.TWILIO_MSID,
             to: process.env.MY_PHONE!, //MY_PHONE이라는 변수가 없을수도있어 뒤에 !를 붙인다   //to: phone
             body: `Your Login Token is ${payload}.` 
+        });
+    }
+    if(email){
+        const transporter = nodemailer.createTransport({
+            service:"gmail",
+            host:"smtp.gmail.com",
+            port:587,
+            auth:{
+                user:process.env.GMAIL,
+                pass:process.env.PASSWORD
+            }
+        });
+        await transporter.sendMail({
+            from:process.env.GMAIL,
+            to:email,
+            subject:"Carrot Market Confirm Auth",
+            html:`Your token is ${payload}.`
         });
     }
 
