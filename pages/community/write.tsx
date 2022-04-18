226498b02fb7c26f99efb0a6a8c03f0264875df4
sinbox0701 <psh090953@gmail.com>
2022-04-18 13:19:26 +0900

    useCoords

--- pages/community/write.tsx
+++ pages/community/write.tsx
@@ -7,6 +7,7 @@ import useMutation from "@libs/client/useMutation";
 import { Post } from "@prisma/client";
 import { useEffect } from "react";
 import { useRouter } from "next/router";
+import useCoords from "@libs/client/useCoords";
 
 interface WriteForm {
     question: string;
@@ -18,12 +19,13 @@ interface WriteMutation {
 }
 
 const Write: NextPage = () => {
+    const {latitude, longitude} = useCoords();
     const router = useRouter();
     const { register, handleSubmit } = useForm<WriteForm>();
     const [ post, {loading, data} ] = useMutation("/api/posts");
     const onValid = (data:WriteForm) => {
         if(loading) return;
-        post(data);
+        post({...data, latitude, longitude});
     };
     useEffect(()=>{
         if(data?.ok){
