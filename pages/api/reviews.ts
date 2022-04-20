45063ebe862141183b4908260b9fa8a327608a9f
sinbox0701 <psh090953@gmail.com>
2022-04-20 10:01:51 +0900

    Profile Handler

---
+++ pages/api/reviews.ts
@@ -0,0 +1,35 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+    req:NextApiRequest,
+    res:NextApiResponse<ResponseType>
+){
+    const {
+        session: {user}
+    } = req;
+
+    const reviews = await client.review.findMany({
+        where:{
+            createdForId: user?.id
+        },
+        include:{
+            createdBy:{
+                select:{
+                    id:true,
+                    name:true,
+                    avatar:true
+                }
+            }
+        }
+    })
+
+    res.json({
+        ok:true,
+        reviews
+    })
+};
+
+export default withApiSession(withHandler({ methods: ["GET"], handler}))
---
+++ pages/api/users/me/purchases.ts
@@ -0,0 +1,29 @@
+import client from "@libs/server/client";
+import withHandler, { ResponseType } from "@libs/server/withHandler";
+import { withApiSession } from "@libs/server/withSession";
+import { NextApiRequest, NextApiResponse } from "next";
+
+async function handler(
+    req:NextApiRequest,
+    res:NextApiResponse<ResponseType>
+){
+    const {
+        session: {user}
+    } = req;
+
+    const purchases = await client.purchase.findMany({
+        where:{
+            userId:user?.id
+        },
+        include:{
+            product:true
+        }
+    });
+
+    res.json({
+        ok:true,
+        purchases
+    })
+};
+
+export default withApiSession(withHandler({ methods: ["GET"], handler}))
