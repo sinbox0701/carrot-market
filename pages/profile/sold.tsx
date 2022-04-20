a55c2d217beb1e9d14efd8e36a91c78da11861be
sinbox0701 <psh090953@gmail.com>
2022-04-20 11:05:21 +0900

    Sold Bought Loved

--- pages/profile/sold.tsx
+++ pages/profile/sold.tsx
@@ -1,21 +1,12 @@
 import type { NextPage } from "next";
 import Layout from "@components/layout";
-import Item from "@components/item";
+import ProductList from "@components/product-list";
 
 const Sold: NextPage = () => {
     return (
-        <Layout title="판매목록" canGoBack>
+        <Layout title="판매내역" canGoBack>
             <div className="flex flex-col space-y-5 pb-10  divide-y">
-                {[...Array(10)].map((_, i) => (
-                    <Item
-                        key={i}
-                        id={i}
-                        title="iPhone 14"
-                        price={99}
-                        comments={1}
-                        hearts={1}
-                    />
-                ))}
+                <ProductList kind="sales" />
             </div>
         </Layout>
     );
