b4aa93c5cab6726be3a9b2fa7d18e0101d37cafe
sinbox0701 <psh090953@gmail.com>
2022-04-24 17:55:22 +0900

    Image Preview

--- pages/profile/edit.tsx
+++ pages/profile/edit.tsx
@@ -4,13 +4,14 @@ import Input from "@components/input";
 import Layout from "@components/layout";
 import useUser from "@libs/client/useUser";
 import { useForm } from "react-hook-form";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import useMutation from "@libs/client/useMutation";
 
 interface EditProfileForm {
     email?: string;
     phone?: string;
     name?: string;
+    avatar?: FileList;
     formErrors?: string;
 };
 
@@ -21,8 +22,8 @@ interface EditProfileResponse {
 
 const Edit: NextPage = () => {
     const { user } = useUser();
-    const { register, handleSubmit, setValue, setError, formState:{errors}} = useForm<EditProfileForm>();
-
+    const { register, handleSubmit, setValue, setError, watch, formState:{errors}} = useForm<EditProfileForm>();
+    //watch --> 미리보기
     useEffect(()=>{
         if(user?.name){
             setValue("name",user.name);
@@ -37,7 +38,7 @@ const Edit: NextPage = () => {
 
     const [editProfile, {data,loading}] = useMutation<EditProfileResponse>(`/api/users/me`);
 
-    const onValid = ({email, phone, name}:EditProfileForm) => {
+    const onValid = ({email, phone, name, avatar}:EditProfileForm) => {
         if(email === "" && phone === "" && name === ""){
             setError("formErrors",{
                 message:"두개중에 하나는 적어야지 양싱없는 인간아!"
@@ -55,14 +56,28 @@ const Edit: NextPage = () => {
         }
     }, [data, setError]);
 
+    const [avatarPreview, setAvatarPreview] = useState("");
+    const avatar = watch("avatar");
+    useEffect(()=>{
+        if(avatar && avatar.length >0){
+            const file = avatar[0];
+            setAvatarPreview(URL.createObjectURL(file));
+        }
+    },[avatar]);
+
     return (
         <Layout canGoBack title="Edit Profile">
             <form className="py-10 px-4 space-y-4" onSubmit={handleSubmit(onValid)}>
                 <div className="flex items-center space-x-3">
-                    <div className="w-14 h-14 rounded-full bg-slate-500" />
+                    {avatarPreview ? (
+                            <img src={avatarPreview} className="w-14 h-14 rounded-full bg-slate-500" />
+                        ): (
+                            <div className="w-14 h-14 rounded-full bg-slate-500" />
+                        )
+                    }
                     <label htmlFor="picture" className="cursor-pointer py-2 px-3 border border-gray-300 rounded-md shadow-sm text-sm font-medium focus:ring-2 focus:ring-offset-2 focus:ring-orange-500 text-gray-700">
                         Change
-                        <input id="picture" type="file" className="hidden" accept="image/*" />
+                        <input {...register("avatar")} id="picture" type="file" className="hidden" accept="image/*" />
                     </label>
                 </div>
                 <Input 
