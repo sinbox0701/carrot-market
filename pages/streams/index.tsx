ff75ef1ce32590f56f8b9f890add4fad27563627
sinbox0701 <psh090953@gmail.com>
2022-05-09 14:11:30 +0900

    마무리

--- pages/streams/index.tsx
+++ pages/streams/index.tsx
@@ -6,6 +6,7 @@ import { Stream } from "@prisma/client";
 import useSWRInfinite from "swr/infinite";
 import { useEffect } from "react";
 import { useInfiniteScroll } from "@libs/client/useInfiniteScroll";
+import Image from "next/image";
 
 interface StreamsResponse {
     ok:boolean;
@@ -34,7 +35,13 @@ const Live: NextPage = () => {
                 {streams?.map((stream) => (
                     <Link key={stream.id} href={`/streams/${stream?.id}`}>
                         <a className="pt-4 block  px-4">
-                            <div className="w-full rounded-md shadow-sm bg-slate-300 aspect-video" />
+                        <div className="w-full relative overflow-hidden rounded-md shadow-sm bg-slate-300 aspect-video">
+                            <Image
+                                layout="fill"
+                                src={`https://videodelivery.net/${stream.cloudflareId}/thumbnails/thumbnail.jpg?height=320`}
+                                alt="thumbnail"
+                            />
+                        </div>
                             <h1 className="text-2xl mt-2 font-bold text-gray-900">
                                 {stream?.name}
                             </h1>
