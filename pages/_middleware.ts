8362c7913867e2d8d50456090cac0e7d5547ad99
sinbox0701 <psh090953@gmail.com>
2022-05-30 18:45:57 +0900

    Response & Redirection

--- pages/_middleware.ts
+++ pages/_middleware.ts
@@ -1,6 +1,14 @@
 import type { NextRequest, NextFetchEvent } from "next/server";
+import { NextResponse } from "next/server";
 
 export function middleware(req: NextRequest, ev: NextFetchEvent) {
-    console.log(req.ua);
-    //req.ua 유저의 환경 검색 가능
+    if(req.ua?.isBot){
+        return new Response("Plz don't be a bot. Be human.", { status: 403 })
+    }
+    if (!req.url.includes("/api")) {
+        if (!req.url.includes("/enter") && !req.cookies.carrotsession) {
+            //console.log(req.cookies)로 쿠키이름 확인 --> carrotsession
+            return NextResponse.redirect(`${req.nextUrl.origin}/enter`);
+        }
+    }
 }
